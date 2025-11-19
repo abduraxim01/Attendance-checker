8af5eb289cd25c9c30e72a81a6325ef124015cbd
Abduraxim <150508362+abduraxim01@users.noreply.github.com>
2025-11-19 14:44:15 +0500

    Add files via upload

---
+++ attendance-checker/src/test/java/uz/abduraxim/attendance_checker/AttendanceCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abduraxim.attendance_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/uz/abduraxim/attendance_checker/AttendanceCheckerApplication.java
@@ -0,0 +1,13 @@
+package uz.abduraxim.attendance_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceCheckerApplication.class, args);
+	}
+
+}
